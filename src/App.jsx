197da8024e495f6c8979e2b1296f9e0e7592c418
progali007 <code.progali@gmail.com>
2023-04-29 02:09:11 +0430

    :tada: Start the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Icon from "../public/icon.svg";
+
+function App() {
+  return (
+    <div>
+      <h1>Crypto App</h1>
+      <img src={Icon} alt="icon" width="200"/>
+    </div>
+  );
+}
+
+export default App;
