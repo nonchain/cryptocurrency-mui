01df7eca986ddce0b281413080a7b435fa997f75
progali007 <code.progali@gmail.com>
2023-04-29 03:03:07 +0430

    :sparkles: (App): Setup custom theme file for MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 // Libraries
-import { Box } from "@mui/material";
+import { useMemo, useState } from "react";
+import { Box, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { Outlet } from "react-router-dom";
 // Components
 import Navbar from "./components/navbar/Navbar";
+// Files
+import { customTheme } from "./theme/theme";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  // const colorMode = useMemo(
+  //   () => ({
+  //     toggleColorMode: () => {
+  //       setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  //     },
+  //   }),
+  //   []
+  // );
+
+  const theme = useMemo(() => createTheme(customTheme(mode)), [mode]);
+  
   return (
-    <Box>
-      <Navbar />
-      <Outlet />
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </ThemeProvider>
   );
 }
 
