4ebbd951a8a5a52b61c7b82ee70e3ba8916c8f7f
progali007 <code.progali@gmail.com>
2023-04-29 02:52:47 +0430

    :wrench: (main): Config routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Libraries
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+// Components
+import App from "./App.jsx";
+// Styles
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/currencies",
+        element: <div>All Currencies page</div>,
+      },
+      {
+        path: "/currency-details/:id",
+        element: <div>Currency details page</div>,
+      },
+      {
+        path: "/news",
+        element: <div>News page</div>,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
